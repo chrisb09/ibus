b1bfe219af69ee198678489b81e7884a8728cece
Christian Frederik Brinkmann <christian.brinkmann09@gmail.com>
2019-06-06 13:13:05 +0200

    change gitignore

--- src/de/christian/f/brinkmann/ibus/Main.java
+++ src/de/christian/f/brinkmann/ibus/Main.java
@@ -5,7 +5,6 @@ import java.io.File;
 public class Main {
 
 	public static void main(String[] args) {
-
 		Encoder.encodeDirectoryAlpha(new File("source"), new File("target"));
 		Decoder.decodeDirectory(new File("target"), new File("result"));
 
